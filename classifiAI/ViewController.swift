190fcd57556033ed3f4d1f976ed89ceac98a4c4c
sho40 <kanazawa.sho40@gmail.com>
2019-12-10 02:59:34 +0900

    Initial Commit

---
+++ classifiAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  classifiAI
+//
+//  Created by Sho Kanazawa on 2019/12/10.
+//  Copyright © 2019 Sho Kanazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
